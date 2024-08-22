04b15638447e83c2f8b2b61ea5a4a0b46a09ae80
Muskan771 <163408441+Muskan771@users.noreply.github.com>
2024-08-22 17:14:15 +0530

    message

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)  # For hashed passwords
+
+    def _str_(self):
+        return self.username
